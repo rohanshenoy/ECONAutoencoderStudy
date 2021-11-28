b86622a3290c5c22fe8355fe23f2709cde14f788
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2021-11-28 14:25:53 -0800

    New input dataset

--- fragments/phoCrabConfig.py
+++ fragments/phoCrabConfig.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'AE_photons_3_23_2'
+config.General.requestName = 'AE_photons_11_22_1'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 
@@ -9,10 +9,10 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'produce_ntuple_std_ae_xyseed_reduced_genmatch_v11_cfg.py'
 config.JobType.maxMemoryMB = 4000
 
-config.Data.inputDataset = '/SinglePhoton_PT2to200/Phase2HLTTDRWinter20DIGI-NoPU_110X_mcRun4_realistic_v3-v2/GEN-SIM-DIGI-RAW'
+config.Data.inputDataset = '/SingleElectron_PT2to200/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1_ext2-v1/FEVT'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.outLFNDirBase = '/store/user/cmantill/HGCAL/AE_Jun11/'
+config.Data.outLFNDirBase = '/store/user/rshenoy/HGCAL/AE_Nov22/'
 config.Data.unitsPerJob = 10
 
 config.Data.publication = False
